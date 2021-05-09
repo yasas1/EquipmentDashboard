57624b986b42a04045afb8ceb4214bdd7fbb84e7
yasas1 <32437531+yasas1@users.noreply.github.com>
2021-05-09 18:05:33 +0530

    initialization

---
+++ EquipmentDashboard/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { BarchartComponent } from './Components/charts/barchart/barchart.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    BarchartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
